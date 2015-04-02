616c30cd02a2baea99fbb75f1a06ccfc34b96e9b
anuroopvipin <anuroopvipin@gmail.com>
2015-04-02 18:51:26 +0530

    Update jquery.json2input.js

--- jquery.json2input.js
+++ jquery.json2input.js
@@ -16,3 +16,22 @@ $.fn.json2input = function(o){
 			   this.append("<form>"+e.join(" ")+"</form>");
                return this;
 }}( jQuery ));
+
+(function ( $ ) {    
+$.json2input = function(o){
+                var e=[];
+                (function f(v, k){ 
+                   if (typeof v == "object") {
+                     for (var kp in v) {
+                       if (Object.hasOwnProperty.call(v, kp)) { 
+                         (f(v[kp], k != undefined ? k + "[" + kp + "]" : kp));
+                         }
+                     } 
+                   } 
+                   else { 
+                     e.push("<input type=\"hidden\" name=\""+k+"\" value=\""+v+"\" />");
+                   }
+               })(o);
+
+               return "<form>"+e.join(" ")+"</form>";
+}}( jQuery ));
